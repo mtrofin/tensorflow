openxla/xla

Internal dir restructure
PiperOrigin-RevId: 740110325

third_party/py/python_configure.bzl
@@ -2,7 +2,7 @@
 """
 
 load(
-    "//third_party/remote_config:common.bzl",
+    "@xla//third_party/remote_config:common.bzl",
     "BAZEL_SH",
     "PYTHON_BIN_PATH",
     "PYTHON_LIB_PATH",

third_party/tensorrt/tensorrt_configure.bzl
@@ -7,13 +7,13 @@
 """
 
 load(
-    "//third_party/gpus:cuda_configure.bzl",
+    "@xla//third_party/gpus:cuda_configure.bzl",
     "find_cuda_config",
     "lib_name",
     "make_copy_files_rule",
 )
 load(
-    "//third_party/remote_config:common.bzl",
+    "@xla//third_party/remote_config:common.bzl",
     "config_repo_label",
     "get_cpu_value",
     "get_host_environ",

mtrofin/tensorflow

third_party/py/python_configure.bzl
@@ -2,7 +2,7 @@
 """
 
 load(
-    "//third_party/remote_config:common.bzl",
+    "@local_xla//third_party/remote_config:common.bzl",
     "BAZEL_SH",
     "PYTHON_BIN_PATH",
     "PYTHON_LIB_PATH",

third_party/tensorrt/tensorrt_configure.bzl
@@ -7,13 +7,13 @@
 """
 
 load(
-    "//third_party/gpus:cuda_configure.bzl",
+    "@local_xla//third_party/gpus:cuda_configure.bzl",
     "find_cuda_config",
     "lib_name",
     "make_copy_files_rule",
 )
 load(
-    "//third_party/remote_config:common.bzl",
+    "@local_xla//third_party/remote_config:common.bzl",
     "config_repo_label",
     "get_cpu_value",
     "get_host_environ",