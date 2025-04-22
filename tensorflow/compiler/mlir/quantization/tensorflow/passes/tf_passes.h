92444e082d168275654db71229effd7dfb047577
Daniel Sosa <sosagarcia@google.com>
2025-04-22 15:28:56 -0700

    Convert 'lift_quantizable_spots_as_functions' pass to TF dialect.
    PiperOrigin-RevId: 750348076

--- tensorflow/compiler/mlir/quantization/tensorflow/passes/tf_passes.h
+++ tensorflow/compiler/mlir/quantization/tensorflow/passes/tf_passes.h
@@ -25,11 +25,30 @@ limitations under the License.
 #include "mlir/IR/BuiltinOps.h"  // from @llvm-project
 #include "mlir/Pass/Pass.h"  // from @llvm-project
 #include "mlir/Support/LLVM.h"  // from @llvm-project
+#include "tensorflow/compiler/mlir/quantization/common/quantization_lib/quantization_config.h"
 #include "tensorflow/compiler/mlir/quantization/stablehlo/quantization_config.pb.h"
 #include "tensorflow/compiler/mlir/quantization/tensorflow/quantization_options.pb.h"
 
 namespace mlir {
 namespace quant {
+
+// Lifts the quantizable spots as composite functions.
+std::unique_ptr<OperationPass<ModuleOp>>
+CreateTFLiftQuantizableSpotsAsFunctionsPass(
+    const tensorflow::quantization::QuantizationOptions& quant_options);
+
+// Creates an instance of the PrepareQuantize pass, which will perform similar
+// transformations as TFL::PrepareQuantizePass.
+std::unique_ptr<OperationPass<func::FuncOp>> CreateTFPrepareQuantizePass(
+    const QuantizationSpecs& quant_specs,
+    tensorflow::quantization::QuantizationMethod::PresetMethod
+        quantization_method);
+
+// Creates an instance of the PrepareQuantizeDRQ pass, which will
+// perform similar transformations as TFL::PrepareQuantizeDynamicRangePass.
+std::unique_ptr<OperationPass<ModuleOp>> CreateTFPrepareQuantizeDRQPass(
+    const QuantizationSpecs& quant_specs,
+    tensorflow::quantization::OpSet op_set);
 
 // Converts FakeQuant ops to quant.qcast and quant.dcast (QDQ) pairs.
 std::unique_ptr<OperationPass<func::FuncOp>>
